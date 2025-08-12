a849552f5afdb39951287d2009fc72ba9c90afd2
Bertrand Cachet <bertrand.cachet@gmail.com>
2025-08-12 12:15:34 +0200

    fixup! fixup! fixup! Add test workflow

--- test/clojure/test_specific_version.sh
+++ test/clojure/test_specific_version.sh
@@ -4,6 +4,6 @@ set -e
 
 source dev-container-features-test-lib
 
-check "chezmoi version is equal to 1.12.0" sh -c "clojure --version | grep '1.12.0'"
+check "clojure version is equal to 1.12.1.1550" sh -c "clojure --version | grep '1.12.1.1550'"
 
 reportResults
